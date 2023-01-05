fd2f00f4b39e98388d0ee000cfecdb5f6d8fe135
gamagegit <cgamage92@yahoo.com>
2023-01-05 07:46:01 -0800

    finished first

---
+++ running_sum_1d_array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def runningSum(self, nums):
+        output = []
+        sum = 0
+        for i in nums:
+            sum += i
+            output.append(sum)
+            
+        return(output)
+        
+        
+
+
+obj = Solution()
+
+
+nums = [1,2,3,4]
+
+print(obj.runningSum(nums))
