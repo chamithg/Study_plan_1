7806795fbdc2a4a9aa292f1f54e5d7695ac2e18a
gamagegit <cgamage92@yahoo.com>
2023-01-17 19:19:39 -0800

    day 13 completed

---
+++ 424_longest_repeting_char.py
@@ -0,0 +1,15 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        maxRep = 0
+        st = 0
+        end = 1
+        
+    
+            
+            
+        
+
+s = "AABABBA"
+k = 1
+obj = Solution()
+print(obj.characterReplacement(s,k))
